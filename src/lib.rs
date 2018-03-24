6c70b50e44deaacf009fcffb709699faa981abfa
Turing Eret <eret@lambdacalcul.us>
2018-03-23 18:06:05 -0600

    Add some basic documentation and some basic denies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,47 @@
+//! A partial function of type `PartialFn<A,B>` is a unary function whose domain is a subset of A.
+//! In addition to being able to call a `PartialFn`, a method `is_defined_at` is provided in
+//! order to test whether the given `PartialFn` is defined at a particular value.
+
+#![deny(missing_docs)]
+#![deny(missing_copy_implementations)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unsafe_code)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
+
+#![cfg_attr(not(feature = "unstable"), deny(unstable_features))]
 #![cfg_attr(feature = "unstable", feature(fn_traits))]
 #![cfg_attr(feature = "unstable", feature(macro_at_most_once_rep))]
 #![cfg_attr(feature = "unstable", feature(unboxed_closures))]
 
+/// The `PartialFn` type.
 pub struct PartialFn<'a, A, B> {
-	__call_fn: Box<Fn(A) -> Option<B> + 'a>,
-	__is_defined_at_fn: Box<Fn(A) -> bool + 'a>
+    __call_fn: Box<Fn(A) -> Option<B> + 'a>,
+    __is_defined_at_fn: Box<Fn(A) -> bool + 'a>,
 }
 
 impl<'a, A, B> PartialFn<'a, A, B> {
-	pub fn call(&self, arg: A) -> Option<B> {
-		(*self.__call_fn)(arg)
-	}
+    /// TODO: call documentation
+    pub fn call(&self, arg: A) -> Option<B> {
+        (*self.__call_fn)(arg)
+    }
 
-	pub fn is_defined_at(&self, arg: A) -> bool {
-		(*self.__is_defined_at_fn)(arg)
-	}
+    /// TODO: is_defined_at documentation
+    pub fn is_defined_at(&self, arg: A) -> bool {
+        (*self.__is_defined_at_fn)(arg)
+    }
 
-	pub fn new(call: Box<Fn(A) -> Option<B> + 'a>, defined: Box<Fn(A) -> bool + 'a>) -> PartialFn<'a, A, B> {
-		PartialFn {
-			__call_fn: call,
-			__is_defined_at_fn: defined
-		}		
-	}
+    #[doc(hidden)]
+    pub fn new(
+        call: Box<Fn(A) -> Option<B> + 'a>,
+        defined: Box<Fn(A) -> bool + 'a>,
+    ) -> PartialFn<'a, A, B> {
+        PartialFn {
+            __call_fn: call,
+            __is_defined_at_fn: defined,
+        }
+    }
 }
 
 #[cfg(feature = "unstable")]
@@ -40,97 +60,97 @@ impl<'a, A, B> FnMut<(A,)> for PartialFn<'a, A, B> {
 
 #[cfg(feature = "unstable")]
 impl<'a, A, B> FnOnce<(A,)> for PartialFn<'a, A, B> {
-	type Output = Option<B>;
+    type Output = Option<B>;
 
     extern "rust-call" fn call_once(self, (arg,): (A,)) -> Self::Output {
-    	(*self.__call_fn)(arg)
+        (*self.__call_fn)(arg)
     }
 }
 
 #[cfg(not(feature = "unstable"))]
 #[macro_export]
+#[doc(hidden)]
 macro_rules! __call_macro (
-	($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
-		|arg| {
-			match arg {
-				$(
-					$($pat)|+ $(if $cond)* => Some($result)
-				),*,
-				_ => None
-			}
-		}
-	);
+    ($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
+        |arg| {
+            match arg {
+                $(
+                    $($pat)|+ $(if $cond)* => Some($result)
+                ),*,
+                _ => None
+            }
+        }
+    );
+);
+
+#[cfg(feature = "unstable")]
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __call_macro (
+    ($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
+        |arg| {
+            match arg {
+                $(
+                    $($pat)|+ $(if $cond)? => Some($result)
+                ),*,
+                _ => None
+            }
+        }
+    );
 );
 
 #[cfg(not(feature = "unstable"))]
 #[macro_export]
+#[doc(hidden)]
 macro_rules! __is_defined_at_macro (
-	($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
-		#[allow(unused_variables)]
-		|arg| {
-			match arg {
-				$(
-					$($pat)|+ $(if $cond)* => true
-				),*,
-				_ => false
-			}
-		}
-	);
+    ($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
+        #[allow(unused_variables)]
+        |arg| {
+            match arg {
+                $(
+                    $($pat)|+ $(if $cond)* => true
+                ),*,
+                _ => false
+            }
+        }
+    );
+);
+
+#[cfg(feature = "unstable")]
+#[macro_export]
+#[doc(hidden)]
+macro_rules! __is_defined_at_macro (
+    ($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
+        #[allow(unused_variables)]
+        |arg| {
+            match arg {
+                $(
+                    $($pat)|+ $(if $cond)? => true
+                ),*,
+                _ => false
+            }
+        }
+    );
 );
 
 #[cfg(not(feature = "unstable"))]
 #[macro_export]
 macro_rules! partial_function (
-	($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
-		PartialFn::new(Box::new(__call_macro!($($($pat)|+ $(if $cond)* => $result),*)), Box::new(__is_defined_at_macro!($($($pat)|+ $(if $cond)* => $result),*)))
-	);
-);
-
-#[cfg(feature = "unstable")]
-#[macro_export]
-macro_rules! __call_macro (
-	($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
-		|arg| {
-			match arg {
-				$(
-					$($pat)|+ $(if $cond)? => Some($result)
-				),*,
-				_ => None
-			}
-		}
-	);
-);
-
-#[cfg(feature = "unstable")]
-#[macro_export]
-macro_rules! __is_defined_at_macro (
-	($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
-		#[allow(unused_variables)]
-		|arg| {
-			match arg {
-				$(
-					$($pat)|+ $(if $cond)? => true
-				),*,
-				_ => false
-			}
-		}
-	);
+    ($($($pat:pat)|+ $(if $cond:expr)* => $result:expr),*) => (
+        PartialFn::new(
+            Box::new(__call_macro!($($($pat)|+ $(if $cond)* => $result),*)),
+            Box::new(__is_defined_at_macro!($($($pat)|+ $(if $cond)* => $result),*))
+        )
+    );
 );
 
 #[cfg(feature = "unstable")]
 #[macro_export]
 macro_rules! partial_function (
-	($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
-		PartialFn::new(Box::new(__call_macro!($($($pat)|+ $(if $cond)? => $result),*)), Box::new(__is_defined_at_macro!($($($pat)|+ $(if $cond)? => $result),*)))
-	);
+    ($($($pat:pat)|+ $(if $cond:expr)? => $result:expr),*) => (
+        PartialFn::new(
+            Box::new(__call_macro!($($($pat)|+ $(if $cond)? => $result),*)),
+            Box::new(__is_defined_at_macro!($($($pat)|+ $(if $cond)? => $result),*))
+        )
+    );
 );
-
-fn handles_pattern_guards() {
-	let c1 = 1;
-	let c2 = 2;
-
-	let pf : PartialFn<i32, i32> = partial_function! {
-		a if a == 1 => a + c1,
-		a if a == 2 => a + c2
-	};
-}
--- tests/test.rs
+++ tests/test.rs
@@ -1,139 +1,147 @@
 #![cfg_attr(feature = "unstable", feature(macro_at_most_once_rep))]
 
-#[macro_use] extern crate partial_function;
+#[macro_use]
+extern crate partial_function;
 
 use partial_function::PartialFn;
 
 #[derive(Debug)]
 enum Test {
-	Foo(i32),
-	Bar(f32),
-	Baz(bool)
+    Foo(i32),
+    Bar(f32),
+    Baz(bool),
 }
 
 #[cfg(not(feature = "unstable"))]
 macro_rules! test_partial_function (
-	($var:expr, $lhs:expr, None) => (
-		assert!(!$var.is_defined_at($lhs) && $var.call($lhs).is_none())
-	);
-	($var:expr, $lhs:expr, $rhs:expr) => (
-		assert!($var.is_defined_at($lhs) && $var.call($lhs).is_some() && $var.call($lhs) == Some($rhs))
-	);
+    ($var:expr, $lhs:expr, None) => (
+        assert!(!$var.is_defined_at($lhs) && $var.call($lhs).is_none())
+    );
+    ($var:expr, $lhs:expr, $rhs:expr) => (
+        assert!($var.is_defined_at($lhs) && $var.call($lhs).is_some() && $var.call($lhs) == Some($rhs))
+    );
 );
 
 #[cfg(feature = "unstable")]
 macro_rules! test_partial_function (
-	($var:expr, $lhs:expr, None) => (
-		assert!(!$var.is_defined_at($lhs) && $var($lhs).is_none())
-	);
-	($var:expr, $lhs:expr, $rhs:expr) => (
-		assert!($var.is_defined_at($lhs) && $var($lhs).is_some() && $var($lhs) == Some($rhs))
-	);
+    ($var:expr, $lhs:expr, None) => (
+        assert!(!$var.is_defined_at($lhs) && $var($lhs).is_none())
+    );
+    ($var:expr, $lhs:expr, $rhs:expr) => (
+        assert!($var.is_defined_at($lhs) && $var($lhs).is_some() && $var($lhs) == Some($rhs))
+    );
 );
 
 
 #[cfg(test)]
-
 #[test]
 fn handles_basic_functionality() {
-	let pf = partial_function! {
-		"foo" => 1,
-		"bar" => 2
-	};
+    let pf =
+        partial_function! {
+        "foo" => 1,
+        "bar" => 2
+    };
 
-	test_partial_function!(pf, "foo", 1);
-	test_partial_function!(pf, "bar", 2);
-	test_partial_function!(pf, "baz", None);
+    test_partial_function!(pf, "foo", 1);
+    test_partial_function!(pf, "bar", 2);
+    test_partial_function!(pf, "baz", None);
 }
 
 #[test]
 fn handles_variable_capture() {
-	let pf = partial_function! {
-		Test::Foo(a) => a,
-		Test::Bar(_b) => 1
-	};
+    let pf =
+        partial_function! {
+        Test::Foo(a) => a,
+        Test::Bar(_b) => 1
+    };
 
-	test_partial_function!(pf, Test::Foo(2), 2);
-	test_partial_function!(pf, Test::Bar(1.0), 1);
-	test_partial_function!(pf, Test::Baz(true), None);
+    test_partial_function!(pf, Test::Foo(2), 2);
+    test_partial_function!(pf, Test::Bar(1.0), 1);
+    test_partial_function!(pf, Test::Baz(true), None);
 }
 
 #[test]
 fn handles_non_local_variable_in_expression() {
-	let c = 1;
+    let c = 1;
 
-	let pf = partial_function! {
-		Test::Foo(a) => a + c
-	};
+    let pf =
+        partial_function! {
+        Test::Foo(a) => a + c
+    };
 
-	test_partial_function!(pf, Test::Foo(1), 2);
-	test_partial_function!(pf, Test::Bar(1.0), None);
+    test_partial_function!(pf, Test::Foo(1), 2);
+    test_partial_function!(pf, Test::Bar(1.0), None);
 }
 
 #[test]
 fn handles_pattern_guards() {
-	let c1 = 1;
-	let c2 = 2;
+    let c1 = 1;
+    let c2 = 2;
 
-	let pf = partial_function! {
-		Test::Foo(a) if a == 1 => a + c1,
-		Test::Foo(a) if a == 2 => a + c2
-	};
+    let pf =
+        partial_function! {
+        Test::Foo(a) if a == 1 => a + c1,
+        Test::Foo(a) if a == 2 => a + c2
+    };
 
-	test_partial_function!(pf, Test::Foo(1), 2);
-	test_partial_function!(pf, Test::Foo(2), 4);
-	test_partial_function!(pf, Test::Foo(3), None);
-	test_partial_function!(pf, Test::Bar(1.0), None);
+    test_partial_function!(pf, Test::Foo(1), 2);
+    test_partial_function!(pf, Test::Foo(2), 4);
+    test_partial_function!(pf, Test::Foo(3), None);
+    test_partial_function!(pf, Test::Bar(1.0), None);
 }
 
 #[test]
 fn handles_pattern_alternation() {
-	let pf = partial_function! {
-		1 | 2 => "foo",
-		3 => "bar"
-	};
+    let pf =
+        partial_function! {
+        1 | 2 => "foo",
+        3 => "bar"
+    };
 
-	test_partial_function!(pf, 1, "foo");
-	test_partial_function!(pf, 2, "foo");
-	test_partial_function!(pf, 3, "bar");
-	test_partial_function!(pf, 4, None);
+    test_partial_function!(pf, 1, "foo");
+    test_partial_function!(pf, 2, "foo");
+    test_partial_function!(pf, 3, "bar");
+    test_partial_function!(pf, 4, None);
 }
 
 #[test]
 fn handles_inclusive_range() {
-	let pf = partial_function! {
-		1...3 => "foo"
-	};
+    let pf =
+        partial_function! {
+        1...3 => "foo"
+    };
 
-	test_partial_function!(pf, 1, "foo");
-	test_partial_function!(pf, 2, "foo");
-	test_partial_function!(pf, 3, "foo");
-	test_partial_function!(pf, 4, None);
+    test_partial_function!(pf, 1, "foo");
+    test_partial_function!(pf, 2, "foo");
+    test_partial_function!(pf, 3, "foo");
+    test_partial_function!(pf, 4, None);
 }
 
 #[test]
 fn handles_pattern_binding() {
-	let pf = partial_function! {
-		a @ "foo" => a
-	};
+    let pf =
+        partial_function! {
+        a @ "foo" => a
+    };
 
-	test_partial_function!(pf, "foo", "foo");
-	test_partial_function!(pf, "bar", None);
+    test_partial_function!(pf, "foo", "foo");
+    test_partial_function!(pf, "bar", None);
 }
 
 #[test]
 fn handles_all_of_it_mixed_together() {
-	let pf = partial_function! {
-		a @ 1...10 | a @ 21...30 if a % 2 == 0 => a/2,
-		a @ 11...20 | a @ 31...40 if a % 2 == 1 => a
-	};
+    let pf =
+        partial_function! {
+        a @ 1...10 | a @ 21...30 if a % 2 == 0 => a/2,
+        a @ 11...20 | a @ 31...40 if a % 2 == 1 => a
+    };
 
-	test_partial_function!(pf, 1, None);
-	test_partial_function!(pf, 2, 1);
-	test_partial_function!(pf, 11, 11);
-	test_partial_function!(pf, 12, None);
-	test_partial_function!(pf, 21, None);
-	test_partial_function!(pf, 22, 11);
-	test_partial_function!(pf, 31, 31);
-	test_partial_function!(pf, 32, None);
+    test_partial_function!(pf, 1, None);
+    test_partial_function!(pf, 2, 1);
+    test_partial_function!(pf, 11, 11);
+    test_partial_function!(pf, 12, None);
+    test_partial_function!(pf, 21, None);
+    test_partial_function!(pf, 22, 11);
+    test_partial_function!(pf, 31, 31);
+    test_partial_function!(pf, 32, None);
 }
